a187f8a6d20f7cea12c62587e85439561a0193e8
almeida-ander <send.almeida@gmail.com>
2026-07-21 17:43:54 -0300

    Test: Debug pubg api part 2

--- api/test.js
+++ api/test.js
@@ -11,10 +11,33 @@ module.exports = async (req, res) => {
         });
         
         const data = await playerRes.json();
-        return res.status(playerRes.status).json({
-            status: playerRes.status,
-            ok: playerRes.ok,
-            data: data
+        const accountId = data.data[0].id;
+        
+        const statsRes = await fetch(`https://api.pubg.com/shards/steam/players/${accountId}/seasons/lifetime`, {
+            headers: {
+                'Authorization': `Bearer ${PUBG_API_KEY}`,
+                'Accept': 'application/vnd.api+json'
+            }
+        });
+        
+        const statsData = await statsRes.json();
+        
+        const weaponRes = await fetch(`https://api.pubg.com/shards/steam/players/${accountId}/weapon_mastery`, {
+                headers: {
+                    'Authorization': `Bearer ${PUBG_API_KEY}`,
+                    'Accept': 'application/vnd.api+json'
+                }
+            });
+            
+        const weaponData = await weaponRes.json();
+        
+        return res.status(200).json({
+            accountId,
+            statsOk: statsRes.ok,
+            statsStatus: statsRes.status,
+            weaponOk: weaponRes.ok,
+            stats: statsData,
+            weapon: weaponData
         });
     } catch (e) {
         return res.status(500).json({ error: e.message });
